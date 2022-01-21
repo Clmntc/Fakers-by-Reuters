8c664917f91b8b959537394009c787061ef9ec09
ClmntC <clement.chrp@gmail.com>
2022-01-21 09:57:27 +0100

    R

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
+import os
+import pickle
 
+PEOPLE_FOLDER = os.path.join('static')
 
 app = Flask(__name__)
+model = pickle.load(open('C:/Users/Utilisateur/Desktop/Arturo/NLP_Fake_news/model_final.pkl', 'rb'))
+
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
 @app.route('/')
 def index():
