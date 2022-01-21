49067d579db26d415c7859921afcd0696ea38648
ClmntC <clement.chrp@gmail.com>
2022-01-21 12:17:42 +0100

    R

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def predict():
     fake_news_img = os.path.join(app.config['UPLOAD_FOLDER'], 'Fake_news.png')
     real_news_img = os.path.join(app.config['UPLOAD_FOLDER'], 'Real_news.png')
     nom = "".join([word.lower() for word in nom if word not in string.punctuation])
-    tokens = word_tokenize(nom)
+    # tokens = word_tokenize(nom)
     # nom = " ".join([ps.stem(word) for word in tokens if word not in stopwords_En])
     input = [nom]
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(input))
