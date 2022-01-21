e73daba51bb98aef0cb109b65e953bc21492c614
ClmntC <clement.chrp@gmail.com>
2022-01-21 10:27:45 +0100

    R

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from flask import Flask, request, render_template
 import os
 import pickle
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-stopwords_En = nltk.corpus.stopwords.words('english')
-ps = nltk.PorterStemmer()
-# Modèle + Matrice de fonctionnalités TF-IDF
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(stop_words='english',max_df=0.7)
