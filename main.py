18b50784ca37248d4e19ca2946dea347fd469270
ClmntC <clement.chrp@gmail.com>
2022-01-21 09:59:56 +0100

    R

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pickle
 PEOPLE_FOLDER = os.path.join('static')
 
 app = Flask(__name__)
-model = pickle.load(open('C:/Users/Utilisateur/Desktop/Arturo/NLP_Fake_news/model_final.pkl', 'rb'))
+model = pickle.load(open('model_final.pkl', 'rb'))
 
 app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 
